9ec59e633efb4d511b498addc7a673cdcfd1bf99
daishi <130416461+ousllc@users.noreply.github.com>
2024-07-19 17:33:30 +0900

    bugfix: smoothscroll

--- src/tableofcontents.js
+++ src/tableofcontents.js
@@ -81,10 +81,9 @@ class Toc {
                 const header = document.querySelector(`#${h.id}`);
                 const headerTop = header.getBoundingClientRect().top + window.pageYOffset;
                 const offsetTop = headerTop - this.options.offsetTop;
-                window.scrollTo({
-                    top: offsetTop,
-                    behavior: 'smooth'
-                });
+
+                // Use the smoothScroll method instead of window.scrollTo
+                this.smoothScroll(offsetTop, this.options.duration);
             });
             tocItem.appendChild(tocLink);
 
@@ -157,6 +156,29 @@ class Toc {
             showAllButton.style.display = 'block';
         }
     }
+    smoothScroll(targetPosition, duration) {
+        const startPosition = window.pageYOffset;
+        const distance = targetPosition - startPosition;
+        let startTime = null;
+
+        function animation(currentTime) {
+            if (startTime === null) startTime = currentTime;
+            const timeElapsed = currentTime - startTime;
+            const run = ease(timeElapsed, startPosition, distance, duration);
+            window.scrollTo(0, run);
+            if (timeElapsed < duration) requestAnimationFrame(animation);
+        }
+
+        function ease(t, b, c, d) {
+            t /= d / 2;
+            if (t < 1) return c / 2 * t * t + b;
+            t--;
+            return -c / 2 * (t * (t - 2) - 1) + b;
+        }
+
+        requestAnimationFrame(animation);
+    }
+
 }
 
 // グローバル関数として提供
